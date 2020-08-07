298b6ca59b2544fa807a2b5949aee3549d460eb4
Yuliya Dzemidovich <yuliya.dzemidovich@gmail.com>
2020-08-07 23:36:19 +0300

    added separate sequence generator for `client` table

--- src/main/java/model/Client.java
+++ src/main/java/model/Client.java
@@ -1,6 +1,16 @@
 package model;
 
-import javax.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
@@ -8,7 +18,16 @@ import javax.persistence.*;
 @DiscriminatorColumn(discriminatorType = DiscriminatorType.STRING, name = "type")
 public class Client {
     @Id
-    @GeneratedValue
+    @GeneratedValue (generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @Parameter(name = "sequence_name", value = "sequence_client_id"),
+                    @Parameter(name = "initial_value", value = "1"),
+                    @Parameter(name = "increment_size", value = "1")
+            }
+    )
     private long id;
 
     public Client(){
